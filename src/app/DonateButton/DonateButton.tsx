ee5b721873648628713f246c7aadefdbf26d051b
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2026-09-18 07:57:43 +0700

    fix: button padding

--- src/app/DonateButton/DonateButton.tsx
+++ src/app/DonateButton/DonateButton.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/app/Button';
 import cfg from '../../../donation-config.json';
+import styles from './DonateButton.module.css';
 
 export function DonateButton() {
   return (
     <Button
+      className={styles.donateButton}
       href={`https://donate.freecodecamp.org?source=${cfg.donationId}&campaign=test-2026&medium=web`}
       variant="cta"
       target_blank
